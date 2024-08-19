191e5a40d800df5305bdffdae9b7533eb0134f6e
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-08-19 20:32:10 +0200

    Implement the PUT operation

--- FruitWebApp/Pages/Edit.cshtml.cs
+++ FruitWebApp/Pages/Edit.cshtml.cs
@@ -8,7 +8,7 @@ using System.Diagnostics;
 
 namespace FruitWebApp.Pages
 {
-	public class EditModel : PageModel
+    public class EditModel : PageModel
     {
         // IHttpClientFactory set using dependency injection 
         private readonly IHttpClientFactory _httpClientFactory;
@@ -19,30 +19,33 @@ namespace FruitWebApp.Pages
         [BindProperty]
         public FruitModel FruitModels { get; set; }
 
-
-        // Retrieve the data to populate the form for editing
-        public async Task OnGet(int id)
+        public async Task<IActionResult> OnPost()
         {
- 
+            // Serialize the information to be edited in the database
+            var jsonContent = new StringContent(JsonSerializer.Serialize(FruitModels),
+                Encoding.UTF8,
+                "application/json");
+
             // Create the HTTP client using the FruitAPI named factory
             var httpClient = _httpClientFactory.CreateClient("FruitAPI");
 
-            // Retrieve record information to populate the form
-            using HttpResponseMessage response = await httpClient.GetAsync(id.ToString());
+            // Execute the PUT request and store the response. The parameters in PutAsync 
+            // appends the item Id to the base address and passes the serialized data to the API
+            using HttpResponseMessage response = await httpClient.PutAsync(FruitModels.id.ToString(), jsonContent);
 
+            // Return to the home (Index) page and add a temporary success/failure 
+            // message to the page.
             if (response.IsSuccessStatusCode)
             {
-                // Deserialize the response to populate the form
-                using var contentStream = await response.Content.ReadAsStreamAsync();
-                FruitModels = await JsonSerializer.DeserializeAsync<FruitModel>(contentStream);
+                TempData["success"] = "Data was edited successfully!";
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                TempData["failure"] = "Operation was not successful...";
+                return RedirectToPage("Index");
             }
         }
-		
-
-		// Begin PUT operation code
-        
-        // End PUT operation code
-
-	}
+    }
 }
 
