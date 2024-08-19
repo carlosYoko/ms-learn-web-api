a447c6ee701d81b0d2302201a09909673cecccdd
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-08-19 20:43:37 +0200

    OnGet method in Edit

--- FruitWebApp/Pages/Edit.cshtml.cs
+++ FruitWebApp/Pages/Edit.cshtml.cs
@@ -19,6 +19,24 @@ namespace FruitWebApp.Pages
         [BindProperty]
         public FruitModel FruitModels { get; set; }
 
+        // Retrieve the data to populate the form for deletion
+        public async Task OnGet(int id)
+        {
+
+            // Create the HTTP client using the FruitAPI named factory
+            var httpClient = _httpClientFactory.CreateClient("FruitAPI");
+
+            // Retrieve record information
+            using HttpResponseMessage response = await httpClient.GetAsync(id.ToString());
+
+            if (response.IsSuccessStatusCode)
+            {
+                // Deserialize the response to populate the form
+                using var contentStream = await response.Content.ReadAsStreamAsync();
+                FruitModels = await JsonSerializer.DeserializeAsync<FruitModel>(contentStream);
+            }
+        }
+
         public async Task<IActionResult> OnPost()
         {
             // Serialize the information to be edited in the database
